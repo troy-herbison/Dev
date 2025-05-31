94a0571a83d7f25391b4a585cd1dfa0cab84e5ee
Troy Herbison <troy.herbison@gmail.com>
2025-05-31 08:14:38 -0500

    Fix not copying sketchybar files in dev.sh

--- dev.sh
+++ dev.sh
@@ -2,8 +2,11 @@
 
 set -x
 
-cp .aerospace.toml $HOME/.aerospace.toml
-cp -r sketchybar $HOME/.config/sketchybar
+DEV_DIR="$HOME/workplace/Dev"
+
+cp $DEV_DIR/.aerospace.toml $HOME/.aerospace.toml
+
+rm -rf $HOME/.config/sketchybar && cp -r $DEV_DIR/sketchybar $HOME/.config/sketchybar
 
 aerospace reload-config
 
